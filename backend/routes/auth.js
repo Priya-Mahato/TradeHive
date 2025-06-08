9d44694d80486ad50c96753469721c7aa46e9bc1
Priya Mahato <priyamahato1950@gmail.com>
2025-06-08 18:05:46 +0530

    Changes done in backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const User = require("../model/UsersModel.js");
+const { UsersModel } = require("../model/UsersModel.js");
 
 
 const router = express.Router();
@@ -10,11 +10,12 @@ const JWT_SECRET = process.env.JWT_SECRET || "yoursecretkey";
 // Register
 router.post("/register", async (req, res) => {
   const { name, username, password } = req.body;
-  const existing = await User.findOne({ username });
+
+  const existing = await UsersModel.findOne({ username });
   if (existing) return res.status(400).json({ message: "User exists" });
 
   const hashed = await bcrypt.hash(password, 10);
-  const user = await User.create({ name, username, password: hashed });
+  const user = await UsersModel.create({ name, username, password: hashed });
 
   res.status(201).json({ message: "User registered successfully" });
 });
@@ -22,7 +23,7 @@ router.post("/register", async (req, res) => {
 // Login
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
+  const user = await UsersModel.findOne({ username });
   if (!user) return res.status(401).json({ message: "Invalid credentials" });
 
   const valid = await bcrypt.compare(password, user.password);
